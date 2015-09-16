f09495d0f2cd21fe78e4f7e98ea857a69dfc278b
Greg Walden <greg.walden@makerbot.com>
2015-09-15 23:27:04 -0400

    use gist

--- index.js
+++ index.js
@@ -5,11 +5,6 @@ var request = require('request')
   , ms = require('ms')
   ;
 
-var urls = [
-  'http://www.fandango.com/amcloewslincolnsquare13_aabqi/theaterpage?date=10/2/2015',
-  // 'http://www.fandango.com/amcloewslincolnsquare13_aabqi/theaterpage?date=12/18/2015',
-];
-
 if (process.env.MAILGUN_API_KEY && process.env.MAILGUN_DOMAIN) {
   var mailgun = require('mailgun-js')({
     apiKey: process.env.MAILGUN_API_KEY, 
@@ -18,34 +13,48 @@ if (process.env.MAILGUN_API_KEY && process.env.MAILGUN_DOMAIN) {
 }
 
 function curl() {
-  urls.forEach(function(url) {
-    request(url, function(err, res, body) {
-      if (err) return;
+  request({
+    uri: process.env.GIST_URL,
+    headers: {
+      'User-Agent': process.env.GITHUB_USER
+    },
+    json: true
+  }, function(err, res, body) {
+    if (err) {
+      console.log(err);
+      return;
+    }
 
-      var $ = cheerio.load(body);
-      $('.showtimes-movie-title').each(function() {
-        var title = $(this).text();
-        if (true || title.match(/martian/i) || title.match(/(star\s+wars|awakens)/i)) {
-          mailgun.messages().send({
-            from: process.env.EMAIL_FROM,
-            to: process.env.EMAIL_TO,
-            subject: util.format('Tickets found for %s!', title),
-            text: util.format('<a href="%s">%s</a>', url)
-          }, function (err) {
-            if (err) 
-              console.log(err);
-          });
-        } else {
-          console.log('No match for ' + title);
-        }
+    var urls = JSON.parse(body.files['urls.json'].content);
+
+    urls.forEach(function(url) {
+      request(url, function(err, res, body) {
+        if (err) return;
+
+        var $ = cheerio.load(body);
+        $('.showtimes-movie-title').each(function() {
+          var title = $(this).text();
+          if (title.match(/martian/i) || title.match(/(star\s+wars|awakens)/i)) {
+            mailgun.messages().send({
+              from: process.env.EMAIL_FROM,
+              to: process.env.EMAIL_TO,
+              subject: util.format('Tickets found for %s!', title),
+              text: url
+            }, function (err) {
+              if (err) 
+                console.log(err);
+            });
+          } else {
+            console.log('No match for ' + title);
+          }
+        });
       });
     });
-  });
+  }); 
 }
 
 setInterval(curl, ms('30min'));
 curl();
-
 
 var server = http.createServer(function(req, res) {
   res.end('Get in and get it!');
